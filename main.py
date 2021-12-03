c01d7303d247e4b512d5161613357919d5b0e186
Abhi Ravikiran <abhi.ravikiran@calsoft.com>
2021-12-02 22:58:18 -0800

    removed trash

--- main.py
+++ main.py
@@ -10,32 +10,22 @@ mystring=StringVar()
 myanswer=Entry(root,font=("Helvetica",24),textvariable=mystring)
 myanswer.pack()
 
-# my_label = Label(root,text="",font=("Helvetica",48))
 my_label = Entry(root,text="",font=("Helvetica",48))
 my_label.pack(pady=20)
 
 def myshuffler():
     
-    # List of States
-    states=['Washington', 'Indiana','California','New York','Texas']
-    
-    # Pick random word from list
-    random_word = choice(states)
-    
     # Break string entered
     myinput=mystring.get()
     break_word = list(myinput)
-    # shuffle(break_word)
     
     # Turn shuffled list into word
     fun_word=''
     for letter in break_word:
         fun_word+=getvalue(letter)
         
-    # my_label.config(text=fun_word)
     my_label.delete(0,END)
     my_label.insert(0,fun_word)
-    # myanswer.delete(0,END)
 
 def getvalue(myindex):
     initvalue=myhash[myindex]
