5706c2cb56d336b62852ea2f5c69e74cc2a4a36b
Abhi Ravikiran <abhi.ravikiran@calsoft.com>
2021-12-02 20:32:59 -0800

    init gui

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import tkinter
 from tkinter import *
 import random
+
+
+root=Tk()
+root.title('Character Scrambler')
+root.iconbitmap('D:\Abhishek Ravikiran\OneDrive\My Documents\MyGit\CharacterScrambler\CharacterScrambler.ico')
+root.geometry("600x400+-1900+100")
+
+
+
+root.mainloop()
