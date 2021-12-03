137ba13401fadcf2fde32ae3d9be3b0065342ba4
Abhi Ravikiran <abhi.ravikiran@calsoft.com>
2021-12-02 21:04:27 -0800

    added shuffle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from random import *
 
 
 root=Tk()
@@ -6,6 +7,18 @@ root.title('Character Scrambler')
 root.iconbitmap('D:\Abhishek Ravikiran\OneDrive\My Documents\MyGit\CharacterScrambler\CharacterScrambler.ico')
 root.geometry("600x400+-1900+100")
 
+my_label = Label(root,text="",font=("Helvetica",48))
+my_label.pack()
 
+def shuffle():
+    
+    # List of States
+    states=['Washington', 'Indiana','California','New York','Texas']
+    
+    # Pick random word from list
+    random_word = choice(states)
+    my_label.config(text=random_word)
+    
+shuffle()
 
 root.mainloop()
